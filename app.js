234774ecd02340eb7ddd090a5a2fc7604af013db
Clint Winter <cwinter.web@gmail.com>
2017-08-16 09:20:22 -0400

    .

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // CONFIG
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost/my_blog', {useMongoClient: true});
-mongoose.connect('mongodb://clint:testpassword@ds029466.mlab.com:29466/clint_blog');
+mongoose.connect('mongodb://clint:testpassword@ds029466.mlab.com:29466/clint_blog', {useMongoClient: true});
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(parser.urlencoded({extended: true}));
